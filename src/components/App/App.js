3addb219378714c9efde79e45ca3bda1c00055ac
Ivan Volodin <ivanwolodin@gmail.com>
2022-06-13 20:33:51 +0500

    Add: file structure of the project

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+
+function App() {
+    return (
+        <div className="root">
+            <div className="page">
+                <Header/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import logo from "../../images/logo.svg" ;
+import "./Header.css"
+
+function Header() {
+    return (
+        <header className="header">
+            <img className="header__logo" src={logo} alt="лого" />
+        </header>
+    );
+}
+
+export default Header;
