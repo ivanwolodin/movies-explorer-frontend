baa5b8d8c8ee2d29c54064047e4071f9bd3a465f
Ivan Volodin <ivanwolodin@gmail.com>
2022-06-22 00:29:32 +0500

    Add: portfolio section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,8 +3,8 @@ import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
-import aboutMe from "../AboutMe/AboutMe";
 import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
 
 function Main() {
     return (
@@ -15,6 +15,7 @@ function Main() {
                 <AboutProject/>
                 <Techs/>
                 <AboutMe/>
+                <Portfolio/>
             </div>
         </div>
     );
