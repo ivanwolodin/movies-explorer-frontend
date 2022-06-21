64a1dd278fad7f112071af3ebcb28436d210f01f
Ivan Volodin <ivanwolodin@gmail.com>
2022-06-21 17:52:26 +0500

    Add: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import logo from "../../images/logo.svg" ;
 import "./Header.css"
+import LoginButtons from "../LoginButtons/LoginButtons";
 
 function Header() {
     return (
         <header className="header">
             <img className="header__logo" src={logo} alt="лого" />
+            <LoginButtons />
         </header>
     );
 }
---
+++ src/components/LoginButtons/LoginButtons.js
@@ -0,0 +1,12 @@
+import "../LoginButtons/LoginButtons.css"
+
+function LoginButtons() {
+    return (
+        <div className="login__buttons">
+            <button className="registration__button">Регистрация</button>
+            <button className="login__button">Войти</button>
+        </div>
+    );
+}
+
+export default LoginButtons;
