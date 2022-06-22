4939167a85fab5e9255ff12baf58f5f5c278d603
Ivan Volodin <ivanwolodin@gmail.com>
2022-06-22 15:17:09 +0500

    Add: MoviesCard section

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,18 @@
+import "./MoviesCard.css"
+import movie_image from "../../images/sample_movie.svg" ;
+import like_button from "../../images/like_button.svg"
+
+function MoviesCard() {
+    return (
+        <div className="moviescard">
+            <img className="moviescard__image" src={movie_image} alt="превью фильма" />
+            <div className="moviescard__info">
+                <p className="moviescard__name">33 слова о дизайне</p>
+                <img className="moviescard__likebutton" src={like_button} alt="кнопка лайка" />
+            </div>
+            <p className="moviescard__duration">1ч42м</p>
+        </div>
+    );
+}
+
+export default MoviesCard
---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,12 @@
+import "./MoviesCardList.css"
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+function MoviesCardList() {
+    return (
+        <div className="moviescardlist content_info">
+            <MoviesCard/>
+        </div>
+    );
+}
+
+export default MoviesCardList
