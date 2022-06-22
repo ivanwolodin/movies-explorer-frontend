e912fef84d10d4de23624854ffb72498655444d2
Ivan Volodin <ivanwolodin@gmail.com>
2022-06-22 18:19:00 +0500

    Add: Movies section

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,12 +2,12 @@ import "./MoviesCard.css"
 import movie_image from "../../images/sample_movie.svg" ;
 import like_button from "../../images/like_button.svg"
 
-function MoviesCard() {
+function MoviesCard({title}) {
     return (
         <div className="moviescard">
             <img className="moviescard__image" src={movie_image} alt="превью фильма" />
             <div className="moviescard__info">
-                <p className="moviescard__name">33 слова о дизайне</p>
+                <p className="moviescard__name">{title}</p>
                 <img className="moviescard__likebutton" src={like_button} alt="кнопка лайка" />
             </div>
             <p className="moviescard__duration">1ч42м</p>
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,39 @@
 import "./MoviesCardList.css"
 import MoviesCard from "../MoviesCard/MoviesCard";
 
+const filmsList = [
+    '33 слова о дизайне',
+    'Киноальманах «100 лет дизайна»',
+    'В погоне за Бенкси',
+    'Баския: Взрыв реальности',
+    'Бег это свобода',
+    'Книготорговцы',
+    'Когда я думаю о Германии ночью',
+    'Gimme Danger: История Игги и The Stooges',
+    'Дженис: Маленькая девочка грустит',
+    'Соберись перед прыжком',
+    'Пи Джей Харви: A dog called money',
+    'По волнам: Искусство звука в кино',
+    'Рудбой',
+    'Скейт — кухня',
+    'Война искусств',
+    'Зона'
+]
+
 function MoviesCardList() {
     return (
         <div className="moviescardlist content_info">
-            <MoviesCard/>
+            <div className="moviescardlist__films ">
+                {filmsList.map((card) => (
+                    <MoviesCard
+                        title={card}
+                    />
+                ))}
+
+            </div>
+            <button className="moviescardlist__button">
+                Еще
+            </button>
         </div>
     );
 }
