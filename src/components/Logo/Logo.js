582eb462057e64c8fcc2429e5389403d48334b73
Елена Володина <elenavolodina@macbookair13.local>
2022-07-18 09:36:57 +0500

    Add: link for logo in Header

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import "../Logo/Logo.css";
 import logo from "../../images/logo.svg";
 
+import {Link} from "react-router-dom";
+
 function Logo() {
   return (
-    <>
+    <Link className="link profile__link" to="/">
       <img className="logo" src={logo} alt="лого" />
-    </>
+    </Link>
   );
 }
 
